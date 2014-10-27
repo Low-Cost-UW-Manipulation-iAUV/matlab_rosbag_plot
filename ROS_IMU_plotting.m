beb8c76cb69afcbd9dc39a076a8b467d5d5fe84c
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-10-27 18:27:48 +0000

    second .bag read in the fixed imu update rate mode. Here we assume the update_rate is stable (400Hz) and determine the passed time by deleting the last sequence number from the current on and dividing the whole kascham by update_rate.

--- ROS_IMU_plotting.m
+++ ROS_IMU_plotting.m
@@ -5,13 +5,12 @@ clc
 close all
 %% Load a bag and get information about it
 % Using load() lets you auto-complete filepaths.
-bag = ros.Bag.load('2014-10-27-16-46-29.bag');
+bag = ros.Bag.load('2014-10-27-18-12-49.bag');
 bag.info()
 %% Read all messages on a few topics
 topic1 = '/imu/data';
 topic2 = '/imu/position';
-%[msgs, meta] = bag.readAll({topic1, topic2});
-%fprintf('Read %i messages\n', length(msgs));
+
 %% Re-read msgs on topic1 and get their metadata
 [imu_data, imu_data_meta] = bag.readAll(topic1);
 [imu_position, imu_position_meta] = bag.readAll(topic2);
@@ -29,6 +28,8 @@ figure(213123);
 plot(pulled_down_acc, acceleration(1, :));
 ylim([-5 5]);
 
+
+%% Plot the position
 accessor = @(Vector3) Vector3;
 [position] = ros.msgs2mat(imu_position, accessor); % Convert struct to 3-by-N matrix of linear velcoity
 times_pos = cellfun(@(x) x.time.time, imu_position_meta); % Get timestamps
