969c2bac5e6912247e39eb84feda5103c8bf2022
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-10-28 15:21:38 +0000

    plotting everything and improved

--- ROS_IMU_plotting.m
+++ ROS_IMU_plotting.m
@@ -5,36 +5,88 @@ clc
 close all
 %% Load a bag and get information about it
 % Using load() lets you auto-complete filepaths.
-bag = ros.Bag.load('2014-10-28-14-04-48.bag');
+bag = ros.Bag.load('2014-10-28-14-45-08.bag');
 bag.info()
 %% Read all messages on a few topics
-topic1 = 'imu/data';	% make sure it matches EXACTLY, including all / or without / the data shown in the command window here
-topic2 = 'imu/position';
+topic1 = '/imu/data';	% make sure it matches EXACTLY, including all / or without / the data shown in the command window here
+topic2 = '/imu/deadbanded';
+topic3 = '/imu/position';
+topic4 = '/imu/velocity';
+topic5 = '/imu/velocity_zeroed';
+
 
 %% Re-read msgs on topic1 and get their metadata
-[imu_data, imu_data_meta] = bag.readAll(topic1);
-[imu_position, imu_position_meta] = bag.readAll(topic2);
+[data_1, meta_1] = bag.readAll(topic1);
+[data_2, meta_2] = bag.readAll(topic2);
+[data_3, meta_3] = bag.readAll(topic3);
+[data_4, meta_4] = bag.readAll(topic4);
+[data_5, meta_5] = bag.readAll(topic5);
 
-fprintf('Got %i messages, first one at time %f\n', ...
-length(imu_data), imu_data_meta{1}.time.time);
-
+%% Plot the raw data
 accessor = @(Imu) Imu.linear_acceleration;
-[acceleration] = ros.msgs2mat(imu_data, accessor); % Convert struct to 3-by-N matrix of linear velcoity
-times_acc = cellfun(@(x) x.time.time, imu_data_meta); % Get timestamps
-pulled_down_acc = times_acc-times_acc(1);
+[plot_data_1] = ros.msgs2mat(data_1, accessor); % Convert struct to 3-by-N matrix of linear velcoity
+times_data_1 = cellfun(@(x) x.time.time, meta_1); % Get timestamps
+baseline_time_data_1 = times_data_1-times_data_1(1);
 
-figure(213123);
+figure(1001);
+hold all;
 % Plot linear speed over time
-plot(pulled_down_acc, acceleration);
+plot(baseline_time_data_1, plot_data_1);
+title('Free Acceleration (no g) [m/s^]');
+legend('x','y','z');
 ylim([-5 5]);
-
+hold off;
 
 %% Plot the position
 accessor = @(Vector3) Vector3;
-[position] = ros.msgs2mat(imu_position, accessor); % Convert struct to 3-by-N matrix of linear velcoity
-times_pos = cellfun(@(x) x.time.time, imu_position_meta); % Get timestamps
-pulled_down_pos = times_pos-times_pos(1);
+[plot_data_3] = ros.msgs2mat(data_3, accessor); % Convert struct to 3-by-N matrix of linear velcoity
+times_data_3 = cellfun(@(x) x.time.time, meta_3); % Get timestamps
+baseline_time_data_3 = times_data_3-times_data_3(1);
 
-figure(4352242);
-plot (pulled_down_pos, position);
+figure(1003);
+hold all;
+plot (baseline_time_data_3, plot_data_3);
+title('Position');
+legend('x','y','z');
+hold off;
+
+%% Plot the Velocity
+
+accessor = @(Vector3) Vector3;
+[plot_data_4] = ros.msgs2mat(data_4, accessor); % Convert struct to 3-by-N matrix of linear velcoity
+times_data_4 = cellfun(@(x) x.time.time, meta_4); % Get timestamps
+baseline_time_data_4 = times_data_4-times_data_4(1);
+
+accessor = @(Vector3) Vector3;
+[plot_data_5] = ros.msgs2mat(data_5, accessor); % Convert struct to 3-by-N matrix of linear velcoity
+times_data_5 = cellfun(@(x) x.time.time, meta_5); % Get timestamps
+baseline_time_data_5 = times_data_5-times_data_5(1);
+
+%% plot x
+figure(10041);
+hold all;
+title('Velocity along x axis');
+plot (baseline_time_data_4,plot_data_4(1,:));
+plot (baseline_time_data_5,plot_data_5(1,:));
+legend('velocity (m/s)', 'velocity after zero detection');
+hold off;
+
+%% plot y
+figure(10042);
+hold all;
+title('Velocity along y axis');
+plot (baseline_time_data_4,plot_data_4(2,:));
+plot (baseline_time_data_5,plot_data_5(2,:));
+legend('velocity (m/s)', 'velocity after zero detection');
+hold off;
+
+%% plot z
+figure(10043);
+hold all;
+title('Velocity along z axis');
+plot (baseline_time_data_4,plot_data_4(3,:));
+plot (baseline_time_data_5,plot_data_5(3,:));
+legend('velocity (m/s)', 'velocity after zero detection');
+hold off;
+
 %% Learn more
